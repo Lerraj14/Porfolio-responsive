986c7047c9e1bf1c147f98cfbacab0e99dad7fe7
Lerraj14 <jarreld14@gmail.com>
2023-01-09 17:15:15 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelector(".nav-links");
+function onToggleMenu(e) {
+    console.log(e.name);
+  e.name = e.name === "menu-outline" ? "close" : "menu-outline";
+  console.log(e.name);
+    navLinks.classList.toggle("top-[-400%]");
+    navLinks.classList.toggle("top-[100%]");
+}
