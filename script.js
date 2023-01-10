6895a60bd618413745f822b2467eb99b4e230203
Lerraj14 <jarreld14@gmail.com>
2023-01-10 16:56:40 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const navLinks = document.querySelector(".nav-links");
+const navButton = document.querySelector(".toggle-menu");
 function onToggleMenu(e) {
-    console.log(e.name);
-  e.name = e.name === "menu-outline" ? "close" : "menu-outline";
-  console.log(e.name);
+    // console.log(e.name);
+  e.name = e.name === "menu-outline"? "close-outline" : "menu-outline";
+  // console.log(e.name);
     navLinks.classList.toggle("top-[-400%]");
     navLinks.classList.toggle("top-full");
 }
+function onToggleNavMenu() {
+  console.log(navButton.name);
+  navButton.name=navButton.name==='close-outline'?"menu-outline":"close-outline";
+  console.log(navButton.name);
+  navLinks.classList.toggle("top-[-400%]");
+  navLinks.classList.toggle("top-full");
+}
+
